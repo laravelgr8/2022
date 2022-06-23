28dc6df85e0e2ca048ef94cc79e54da91e785682
laravelgr8 <82206712+laravelgr8@users.noreply.github.com>
2022-06-23 13:10:18 +0530

    Update custom_command.php

--- custom_command.php
+++ custom_command.php
@@ -6,9 +6,18 @@ open your file
 
 protected $signature = 'showDB';  //it is your command name
 
+protected $signature = 'getDB {id}';   //if you want to pass data
+
 public function handle()
 {
     $this->info(DB::connection()->getDatabaseName());
+    
+    //if you want to fetch data
+    $id = $this->argument('id');
+    $data=DB::table('users')->where('id',$id)->get();
+    $this->info($data);
 }
 
 now php artisan showDB
+
+now php artisan showDB 12
