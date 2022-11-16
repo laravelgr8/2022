ef5a706cad146edf3be28e119e1485411ed46fdd
laravelgr8 <82206712+laravelgr8@users.noreply.github.com>
2022-11-16 22:40:23 +0530

    Create Multiple_db_connection.php

---
+++ Multiple_db_connection.php
@@ -0,0 +1,35 @@
+On model
+
+class McibModel extends Model {
+
+    protected $connection= 'second_db_connection';
+
+    protected $table = 'agencies';
+
+}
+
+
+return array(
+    'connections' => array(
+        'mysql' => array(
+            'driver'    => 'mysql',
+            'host'      => 'localhost',
+            'database'  => 'database1',
+            'username'  => 'user1',
+            'password'  => 'pass1'
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+        ),
+
+        'second_db_connection' => array(
+            'driver'    => 'mysql',
+            'host'      => 'localhost',
+            'database'  => 'database2',
+            'username'  => 'user2',
+            'password'  => 'pass2'
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+        ),
+    ),
